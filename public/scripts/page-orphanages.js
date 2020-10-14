ac83490a42e1b3f90bc690b3a30bc429d7f4ff2a
Tchiinhemba <tchiinhemba@outlook.com>
2020-10-14 18:29:39 +0100

    update

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,12 +1,12 @@
 // create map
-const map = L.map('mapid').setView([-8.8178385, 13.2295304], 14.73);
+const map = L.map('mapid').setView([-8.8178385, 13.2295304], 14.78);
 
 // create and add title layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // create icon
 const icon = L.icon({
-    iconUrl: "../images/map-marker.svg",
+    iconUrl: "./public/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
@@ -19,9 +19,9 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('lar das meninas <a href="orphanage.html?id=1">')
+}).setContent('UNICEF Angola <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
 
 // create and add mark
-L.marker([-8.8178385, 13.2295304]).addTo(map)
-    .bindPopup('Ingombota, Luanda.<br> Angola.')
-    .openPopup();
+L.marker([-8.8354864, 13.2283384], { icon })
+    .addTo(map)
+    .bindPopup(popup);
