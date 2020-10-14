e5e8c8eb5e9673f9c055629494bb540d7df4016d
Tchiinhemba <tchiinhemba@outlook.com>
2020-10-14 03:23:10 +0100

    update map

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,10 +1,27 @@
+// create map
+const map = L.map('mapid').setView([-8.8178385, 13.2295304], 14.73);
 
-const map = L.map('mapid').setView([-8.809214, 13.236294], 13);
+// create and add title layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// create icon
+const icon = L.icon({
+    iconUrl: "../images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2];
+})
 
-L.marker([-8.809214, 13.236294]).addTo(map)
+// create popup overlay
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('lar das meninas <a href="orphanage.html?id=1">')
+
+// create and add mark
+L.marker([-8.8178385, 13.2295304]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
