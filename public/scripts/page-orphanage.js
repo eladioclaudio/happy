955a38a9e40cf56b6b7b496f164b954e99bccb58
Tchiinhemba <tchiinhemba@outlook.com>
2020-10-15 16:39:10 +0100

    update

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,28 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+// create map
+const map = L.map('mapid', options).setView([-8.8354864, 13.2283384], 14.78);
+
+// create and add title layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+
+
+// create and add mark
+L.marker([-8.8354864, 13.2283384], { icon })
+    .addTo(map)
