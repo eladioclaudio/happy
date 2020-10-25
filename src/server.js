48d8f8cfb71212fc0d3b12e81732828a34ed0539
Tchiinhemba <tchiinhemba@gmail.com>
2020-10-26 00:26:26 +0100

    inject nodemon dependency

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
+// importar dependencia
 const express = require('express');
+
+// iniciando express
+const server = express();
+
+// criar uma rota
+
+server.get('/', () => {
+    
+})
+
+// ligar o servidor
+server.listen(5500)
