6e17f8dcf593403a1050a941c3fb49bfc27b6a10
Tchiinhemba <tchiinhemba@gmail.com>
2020-10-26 01:04:58 +0100

    inject hbs dependency

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 // importar dependencia
 const express = require('express');
+const path = require('path');
 
 // iniciando express
 const server = express();
 
+// utilizando arquivos estaticos
+server.use(express.static('public'));
+
 // criar uma rota
 
-server.get('/', () => {
-    
+server.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))    
 })
 
 // ligar o servidor
