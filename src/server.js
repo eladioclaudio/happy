8e4459ecbcd4dd4e2aeab89a7bd4d42336cb199f
Tchiinhemba <tchiinhemba@gmail.com>
2020-10-26 20:44:10 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,27 +1,27 @@
 // importar dependencia
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const pages = require('./pages.js');
 
-console.log(pages);
 
 // iniciando express
 const server = express();
 
 // utilizando arquivos estaticos
 server
-  .use(express.static("public"))
+  .use(express.static('public'))
 
   // configurar a template engine
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "hbs");
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'hbs')
 
   // rotas da aplicacao
 server
 .get('/', pages.index)
 .get('/orphanage', pages.orphanage)
 .get('/orphanages', pages.orphanages)
-.get('/create-orphanage', pages.createOrphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
 
 // ligar o servidor
 server.listen(5500);
