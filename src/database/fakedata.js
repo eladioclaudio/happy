7f1ef824e0a72b42f12a19efc4a6e0f91b683366
Tchiinhemba <tchiinhemba@gmail.com>
2020-10-28 00:50:43 +0100

    update

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -19,8 +19,8 @@ module.exports = [
     {
         id: 2,
         lat: "-8.8178385",
-        lng: 13.2295304,
-        name: "UNICEF Luanda",
+        lng: 13.2395304,
+        name: "UNICEF Benguela",
         description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
         images: [
             "https://images.unsplash.com/photo-1573261524302-6f1dc1458378?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
@@ -28,7 +28,6 @@ module.exports = [
         ],
         instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
         opening_hours: "Horário de visitas Das 8h até 18h",
-        open_on_weekends: "1"
-
+        open_on_weekends: "0"
     },
 ]
