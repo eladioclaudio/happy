b42d4b0dc3f1316000a80e7c405e2a25ee5bbd0a
Tchiinhemba <tchiinhemba@gmail.com>
2020-10-28 13:24:39 +0100

    update

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,27 +1,29 @@
 // create map
-const map = L.map('mapid').setView([-8.8178385, 13.2295304], 14.78);
+const map = L.map("mapid").setView([-8.8178385, 13.2295304], 14.78);
 
 // create and add title layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // create icon
 const icon = L.icon({
-    iconUrl: "/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
+  iconUrl: "/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
-// create popup overlay
+function addMarker({id, name, lat, lng}) {
 
-const popup = L.popup({
+  // create popup overlay
+  const popup = L.popup({
     closeButton: false,
-    className: 'map-popup',
+    className: "map-popup",
     minWidth: 240,
-    minHeight: 240
-}).setContent('UNICEF Angola <a href="orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"></a>')
+    minHeight: 240,
+  }).setContent(
+    `UNICEF Angola <a href="orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"></a>`
+  );
 
-// create and add mark
-L.marker([-8.8354864, 13.2283384], { icon })
-    .addTo(map)
-    .bindPopup(popup);
+  // create and add mark
+  L.marker([-8.8354864, 13.2283384], { icon }).addTo(map).bindPopup(popup);
+}
