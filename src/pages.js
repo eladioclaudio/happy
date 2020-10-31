a5be660278949842a6760221ab1b0659176f13a8
Tchiinhemba <tchiinhemba@gmail.com>
2020-10-31 23:09:00 +0100

    update

--- src/pages.js
+++ src/pages.js
@@ -16,6 +16,8 @@ module.exports = {
 
       orphanage.images = orphanage.images.split(',');
       orphanage.firstImage = orphanage.images[0];
+
+      (orphanage.open_on_weekends == "0" ? orphanage.open_on_weekends = false : orphanage.open_on_weekends = true);
       
       return res.render('orphanage', { orphanage });
     } catch(error) {
