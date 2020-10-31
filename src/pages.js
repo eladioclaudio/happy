261eb699f994c32f7de441332fe09ae45b488297
Tchiinhemba <tchiinhemba@gmail.com>
2020-10-31 20:12:27 +0100

    update

--- src/pages.js
+++ src/pages.js
@@ -6,14 +6,24 @@ module.exports = {
       return res.render('index') 
     },
 
-  orphanage(req, res) { 
-      return res.render('orphanage') 
+  async orphanage(req, res) { 
+
+    const id = req.query.id;
+    try {
+      const db = await Database;
+      const orphanage = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`);
+      return res.render('orphanage', { orphanage: orphanage[0] });
+
+    } catch(error) {
+      console.log(error);
+      return res.send('Erro no banco de dados');
+    }
     },
 
   async orphanages(req, res) {
     try {
-      const db = await Database;
-      const orphanages = await db.all('SELECT * FROM orphanages WHERE id = "2"');
+    const db = await Database;
+      const orphanages = await db.all('SELECT * FROM orphanages');
       return res.render('orphanages', { orphanages }) 
 
     } catch(error) {
